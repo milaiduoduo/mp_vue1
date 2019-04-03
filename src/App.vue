191b61f20ce8797219c43545abb1eee1fc07a134
advancingKoala <milaiduoduo4567@163.com>
2019-04-03 19:52:25 +0800

    个人中心登录功能修改，登录是调用wx接口登录。

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,6 @@
 <script>
-import { showSuccess } from "./util"; //优化
-import qcloud from "wafer2-client-sdk";
-import config from "@/config.js";
-export default {
-  async created() {
-    // console.log("app.vue config.loginUrl:", config.loginUrl);
-    qcloud.setLoginUrl(config.loginUrl);
-    const session = qcloud.Session.get();
-    if (session) {
-      qcloud.loginWithCode({
-        success: res => {
-          console.log("没过期的登录", res);
-          this.loginSuccess(res);
-        },
-        fail: err => {
-          console.error("登录失败：", err);
-        }
-      });
-    } else {
-      qcloud.login({
-        success: res => {
-          console.log("登录成功：", res);
-          this.loginSuccess(res);
-        },
-        fail: err => {
-          console.error("登录失败：", err);
-        }
-      });
-    }
-    console.log("app.vue 小程序启动了！");
-  },
-  methods: {
-    loginSuccess(res) {
-      showSuccess("登录成功");
-      wx.setStorageSync("userinfo", res);
-      this.userinfo = res;
-    }
-  }
-};
+
+export default {};
 </script>
 <style>
 .btn {
--- src/pages/Me/Me.vue
+++ src/pages/Me/Me.vue
@@ -1,35 +1,79 @@
 <template>
     <div class="container">
-        <div class="userinfo">
-            <img :src="userinfo.avatarUrl" alt="">
-            <p>{{userinfo.nickName}}</p>
-        </div>
-        <year-progress></year-progress>
-        <button class="btn" @click="scanBooks">添加图书</button>
+      <div class="userinfo">
+        <img class='avatar' :src="userInfo.avatarUrl" alt="">
+        <div>{{userInfo.nickName}}</div>
+      </div>
+      <button v-if="userInfo.openId" @click="scanBook" class='btn'>添加图书</button>
+      <button v-else size='mini' open-type="getUserInfo" @getuserinfo="login">点击登录</button>
     </div>
 </template>
 
 <script>
+import { showSuccess } from "@/util.js"; //优化
+import qcloud from "wafer2-client-sdk";
+import config from "@/config.js";
 export default {
   data() {
     return {
-      userinfo: {
-        avatarUrl: "../../static/img/unlogin.png",
-        nickName: "点击登录"
+      userInfo: {
+        avatarUrl: "http://image.shengxinjing.cn/rate/unlogin.png",
+        nikeName: ""
       }
     };
   },
-  created() {
-    this.userinfo = wx.getStorageSync("userinfo");
-    console.log("Me.vue, this.userinfo:", this.userinfo);
+  onShow() {
+    // wx.showShareMenu();
+    let userInfo = wx.getStorageSync("userinfo");
+    if (userInfo) {
+      this.userInfo = userInfo;
+    }
   },
+  // created() {
+  //   // this.userInfo = wx.getStorageSync("userinfo");
+  //   // console.log("Me.vue, this.userinfo:", this.userInfo);
+  // },
   methods: {
+    login() {
+      wx.showToast({
+        title: "登录中",
+        icon: "loading"
+      });
+      qcloud.setLoginUrl(config.loginUrl);
+      const session = qcloud.Session.get();
+      if (session) {
+        qcloud.loginWithCode({
+          success: res => {
+            console.log("没过期的登录", res);
+            this.loginSuccess(res);
+          },
+          fail: err => {
+            console.error(err);
+          }
+        });
+      } else {
+        qcloud.login({
+          success: res => {
+            console.log("登陆成功！", res);
+            this.loginSuccess(res);
+          },
+          fail: err => {
+            console.error(err);
+          }
+        });
+      }
+    },
     scanBooks() {
       wx.scanCode({
         success(res) {
           console.log("book info: ", res);
         }
       });
+    },
+    loginSuccess(res) {
+      showSuccess("登录成功！");
+      wx.setStorageSync("userinfo", res);
+      this.userInfo = res;
     }
   }
 };

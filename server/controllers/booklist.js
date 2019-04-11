3925d26d1b2ae19964dbc4c3cde6604137e8fe23
advancingKoala <milaiduoduo4567@163.com>
2019-04-11 16:39:17 +0800

    数据库关联查询，根据openId查出并显示“添加人”

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -3,8 +3,15 @@ const {
 } = require('../qcloud');
 
 module.exports = async (ctx) => {
-  const books = await mysql('books').select('*').orderBy('id', 'desc');
+  const books = await mysql('books').select('books.*', 'cSessionInfo.user_info').join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id').orderBy('books.id', 'desc');
   ctx.state.data = {
-    list: books
+    list: books.map(item => {
+      const info = JSON.parse(item.user_info);
+      return Object.assign({}, item, {
+        user_info: {
+          'nickName': info.nickName
+        }
+      })
+    })
   }
 }

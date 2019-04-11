51fe4286197ba4c33bf53a9b08f78f35e1b21d15
advancingKoala <milaiduoduo4567@163.com>
2019-04-11 21:30:24 +0800

    8.6 图书列表下拉触底加载功能，完成后端分页请求的部分。完善前端逻辑处理部分。效果是：从上往下拉，我们让其每一次都从数据库去获取最新的第一页数据；从底部往上拉，我们是去请求后续页面的数据，对当前页面数据做追加，如果没有更多数据仍然发请求（不能不发请求，因为数据库随时可能加入更多的数据），只是追加上来的数据需要跟之前的数据比较，做去重复处理。（去重的工作待处理）。

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -3,7 +3,15 @@ const {
 } = require('../qcloud');
 
 module.exports = async (ctx) => {
-  const books = await mysql('books').select('books.*', 'cSessionInfo.user_info').join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id').orderBy('books.id', 'desc');
+  const {
+    pageNum,
+    pageSize
+  } = ctx.request.query;
+  const books = await mysql('books').select('books.*', 'cSessionInfo.user_info')
+    .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
+    .limit(pageSize)
+    .offset((Number(pageNum) - 1) * pageSize)
+    .orderBy('books.id', 'desc');
   ctx.state.data = {
     list: books.map(item => {
       const info = JSON.parse(item.user_info);
--- src/pages/book/book.vue
+++ src/pages/book/book.vue
@@ -14,26 +14,46 @@ export default {
     return {
       bookList: [],
       pageNum: 1,
-      init: true,
       pageSize: 10,
       more: true
     };
   },
   methods: {
-    async getBookList() {
+    async getBookList(getNew = false) {
       // wx.showNavigationBarLoading();
-      const books = await get(config.bookList, {
-        pageNum: this.pageNum,
-        size: this.pageSize
-      });
-      if (
-        books.list.length < this.pageSize ||
-        books.list.length < this.pageSize
-      ) {
+      let books = [];
+      // getNew参数是为了区分是从上往下拉还是从底部往上拉。getNew=true 表示从上往下拉。
+      // 两者请求数据的方式不一样。
+      // 从上往下拉，我们让其每一次都从数据库去获取最新的第一页数据
+      // 从底部往上拉，我们是去请求后续页面的数据，对当前页面数据做追加。
+      if (getNew) {
+        //getNew表示从数据库获取第一组最新数据,从上往下拉，表示获取当前页第一组数据
+        books = await get(config.bookList, {
+          pageNum: 1,
+          pageSize: this.pageSize
+        });
+      } else {
+        //表示从底部往上拉或者更多数据
+        // if (this.more) {
+        books = await get(config.bookList, {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize
+        });
+        // }
+      }
+
+      if (books.list.length < this.pageSize) {
         //表示没有更多数据，pageNum不需要再增加
         this.more = false;
       }
-      this.bookList = [...this.bookList, ...books.list];
+      if (getNew) {
+        this.bookList = books.list;
+      } else {
+        // 需要解决的场景，第一次加载完成后，数据库中新增了几条数据，这时再触底加载时，此次加载的数据可能包含了第一组加载的数据
+        // 所以后续来的数据需要去重
+        this.bookList = [...this.bookList, ...books.list];
+      }
+
       console.log("booklist:", books.list);
       wx.stopPullDownRefresh();
       // wx.hideNavigationBarLoading();
@@ -45,7 +65,7 @@ export default {
   },
   onPullDownRefresh() {
     console.log("下拉了！");
-    this.getBookList();
+    this.getBookList(true);
   },
   onReachBottom() {
     if (this.more) {

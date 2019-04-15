24986c2ba685b65d69bc53086c7b38467f3b6a16
advancingKoala <milaiduoduo4567@163.com>
2019-04-15 15:36:12 +0800

    9.1,9.2 图书详情页的背景画面和文字。有两个知识点：1、虚化背景 filter:blur(15px)，2、转发的设置，需要在生命周期函数onShow()中调用wx.showShareMenu()。

--- server/controllers/getBookDetail.js
+++ server/controllers/getBookDetail.js
@@ -7,7 +7,21 @@ module.exports = async (ctx) => {
   const {
     id
   } = ctx.request.query;
-  console.log("---------------------id:", id);
-  const info = await mysql('books').where('id', id).increment('count', 1);
-  console.log('---------------------info:', info);
+
+  const addInfo = await mysql('books').where('id', id).increment('count', 1);
+  const detailInfo = await mysql('books')
+    .select('books.*', 'csessioninfo.user_info')
+    .join('csessioninfo', 'books.openid', 'csessioninfo.open_id')
+    .where('id', id)
+    .first()
+
+  const info = JSON.parse(detailInfo.user_info);
+  console.log("---------------detailInfo:", detailInfo);
+  ctx.state.data = Object.assign({}, detailInfo, {
+    summary: detailInfo.summary.split('\n'),
+    user_info: {
+      name: info.nickName,
+      image: info.avatarUrl
+    }
+  })
 }
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,6 +1,14 @@
 <template>
 <div>
-    图书id:{{id}}
+    <div class="bgImgWrap">
+      <img mode="aspectFill" class="bgImg" :src="bookInfo.image">
+      <img mode="aspectFit" class="bookImg"  :src="bookInfo.image" :alt="bookInfo.title">
+      <div class="info">
+        <div class="title">{{bookInfo.title}}</div>
+        <div class="author">{{bookInfo.author}}</div>
+      </div>
+    </div>
+    <button open-type="share">转发</button>
 </div>
 </template>
 
@@ -10,22 +18,61 @@ import config from "@/config";
 export default {
   data() {
     return {
-      id: ""
+      id: "",
+      bookInfo: {}
     };
   },
   mounted() {
     this.id = this.$root.$mp.query.id;
     this.getBookDetail();
+    console.log("这句是同步代码，所以this.bookInfo为空：", this.bookInfo);
+  },
+  onShow() {
+    //要用转发功能，需要手动添加onShow生命周期，调用 wx.showShareMenu({});才可以显示转发。
+    wx.showShareMenu();
   },
   methods: {
     async getBookDetail() {
       //浏览量+1
-      console.log("in getBookDetail:", config.getBookDetail);
-      await get(config.getBookDetail, { id: this.id });
+      // console.log("in getBookDetail:", config.getBookDetail);
+      const bookInfo = await get(config.getBookDetail, { id: this.id });
+      this.bookInfo = bookInfo;
+      //setNavigationBarTitle这句还不能单独写在mounted里，还只能写在async回调里。。。。。。
+      wx.setNavigationBarTitle({ title: this.bookInfo.title });
     }
   }
 };
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
+.bgImgWrap {
+  position: relative;
+  overflow: hidden;
+  .bgImg {
+    width: 750rpx;
+    height: 500rpx;
+    filter: blur(15px);
+  }
+  .bookImg {
+    position: absolute;
+    width: 100%;
+    height: 300rpx;
+    left: 0px;
+    top: 30rpx;
+  }
+  .info {
+    position: absolute;
+    top: 350rpx;
+    left: 0;
+    width: 100%;
+    color: #fff;
+    text-align: center;
+    .title {
+      font-size: 20px;
+    }
+    .author {
+      font-size: 14px;
+    }
+  }
+}
 </style>
